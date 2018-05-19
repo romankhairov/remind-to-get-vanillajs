b37caa6de014eea0907c687c36b41c218fdafd98
Roman <roman.khairov@gmail.com>
2018-05-19 22:33:51 +0200

    first build

--- parcel/index.js
+++ parcel/index.js
@@ -1,1 +1,14 @@
 require ('bulma');
+
+var $addItem = document.querySelector('.add-item');
+var $writeItem = document.querySelector('.write-item');
+
+$addItem.addEventListener('click', function() {
+  var value = $writeItem.value;
+  if (value) addItemTodo(value);
+  // console.log("Test");
+});
+
+function addItemTodo(whatEver) {
+  console.log(whatEver);
+}

a5fbae87622ec6ab0919434c4499c7c92b7c0ca0
Roman <roman.khairov@gmail.com>
2018-06-02 17:15:56 +0200

    add separate objects after input

--- scripts/main.js
+++ scripts/main.js
@@ -4,33 +4,55 @@ const $addItem = document.querySelector('.add-item');
 const $writeItem = document.querySelector('.write-item');
 const $taskList = document.querySelector('.task-list');
 
+
 $addItem.addEventListener('click', function() {
   var value = $writeItem.value;
   if (value) addItemTodo(value);
 });
 
+function removeItem(e) {
+  console.log(e);
+}
+
 function addItemTodo(inputText) {
 
-const itemTemplate = `
-  <li class="panel-block">
-      <div class="container is-fluid">
-          ${inputText}
-      </div>
-      <div class="field is-grouped">
-              <p class="control">
-                <a class="button is-small is-success is-outlined">Done</a>
-              </p>
-              <p class="control">
-                <a class="delete"></a>
-              </p>
-      </div>
-  </li>
-`;
+// const itemTemplate = `
+//   <li class="panel-block">
+//       <div class="container is-fluid">
+//           ${inputText}
+//       </div>
+//   </li>
+// `;
 
 var list = document.getElementById("task-list");
 
-var newItem = document.createElement("li");
-newItem.innerHTML = itemTemplate;
+
+var newItem = document.createElement('li');
+newItem.classList.add('panel-block');
+
+var textBlock = document.createElement('div');
+textBlock.classList.add('container', 'is-fluid');
+textBlock.innerText = inputText;
+
+var buttons = document.createElement('div');
+buttons.classList.add('field', 'is-grouped');
+
+var done = document.createElement('button');
+done.classList.add('button', 'is-small', 'is-success', 'is-outlined');
+done.innerText = "Complete";
+
+var remove = document.createElement('button');
+remove.classList.add('delete', 'remove-btn');
+remove.innerText = "Done";
+
+
+
+buttons.appendChild(done);
+buttons.appendChild(remove);
+newItem.appendChild(textBlock);
+newItem.appendChild(buttons);
+
+
 
 list.insertBefore(newItem, list.childNodes[0]);
 
